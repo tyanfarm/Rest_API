5a0bf4601feb988c8eea4865805aa883ccf9909c
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-05-06 21:17:24 +0700

    add migrations

---
+++ Migrations/20240506141700_Add Players table in DB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rest_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayerstableinDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
